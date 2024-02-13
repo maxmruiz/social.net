1c586e8874eb0b1a6a1175cf57af2f9551d7e9eb
max <mmfern2003@gmail.com>
2024-02-13 13:38:46 -0500

    initialize User model

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function (value) {
+                    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
+                },
+                message: 'Invalid email address'
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            }
+        ]
+    }
+
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
