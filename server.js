34f05741679411406c11b98e75827706aa2a2534
max <mmfern2003@gmail.com>
2024-02-19 10:05:41 -0500

    initialize server connection

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
